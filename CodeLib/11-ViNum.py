8303c77b0f31f290a14b00fd0ab2c4c0111ebd90
v-hegde31 <39787460+v-hegde31@users.noreply.github.com>
2020-02-03 23:59:14 +0530

    Update and rename ViNum.py to 11-ViNum.py

--- CodeLib/11-ViNum.py
+++ CodeLib/11-ViNum.py
@@ -1,3 +1,8 @@
+"""
+Python code to print the sequence of number and number of letters in a given number until a terminating number 4 is reached.
+24-06-2019
+"""
+
 alpha = [4,3,3,5,4,4,3,5,5,4,3,6,6,8,8,7,7,9,8,8]
 ten = [6,6,5,5,5,7,6,6]
 h = [7]
