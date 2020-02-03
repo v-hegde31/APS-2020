9ba75c2ad1e8a931b38092e1b61b9bcef84ef556
v-hegde31 <39787460+v-hegde31@users.noreply.github.com>
2020-02-03 23:57:09 +0530

    Add files via upload

---
+++ CodeLib/ViNum.py
@@ -0,0 +1,25 @@
+alpha = [4,3,3,5,4,4,3,5,5,4,3,6,6,8,8,7,7,9,8,8]
+ten = [6,6,5,5,5,7,6,6]
+h = [7]
+for i in range(1,9):
+    h.append(h[0]+alpha[i+1])  
+for i in range(8):
+        alpha.append(ten[i])
+        for j in range(1,10):
+            alpha.append(ten[i]+alpha[j])            
+for k in range(9):
+    alpha.append(h[k])
+    for a in range(1,20):
+        alpha.append(h[k]+alpha[a])
+    for i in range(8):
+        alpha.append(h[k]+ten[i])
+        for j in range(1,10):
+            alpha.append(h[k]+ten[i]+alpha[j])
+   
+T = int(input())
+for iterations in range(T):
+    N = int(input())
+    while(N!=4):
+        print(alpha[N])
+        N = alpha[N]
+    print(N)
