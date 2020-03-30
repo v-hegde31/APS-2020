f2c418b2fe349caf8d7f180fceab6bc75633efc5
v-hegde31 <39787460+v-hegde31@users.noreply.github.com>
2020-03-30 18:45:02 +0530

    Create 62-Number_of-Strictly-Increasing-Subarrays-BF-O(n**2).py

---
+++ CodeLib/62-Number_of-Strictly-Increasing-Subarrays-BF-O(n**2).py
@@ -0,0 +1,14 @@
+def increasingSubArray(N,A):
+    count = 0
+    for i in range(N):
+        for j in range(i+1,N):
+          if A[j] > A[j-1]:
+              count+=1
+          else:
+              break
+    return count
+
+if "__name__" == "__main__":
+    N = int(input())
+    A = list(map(int,input().split(" ")))
+    increasingSubArray(N,A)
