23288ae1ff918b34a249e6679ed930b78e43c88e
v-hegde31 <39787460+v-hegde31@users.noreply.github.com>
2020-03-30 18:57:21 +0530

    Create 63-Number-of-Strictly-Increasing-SubArrays-O(n).py

---
+++ CodeLib/63-Number-of-Strictly-Increasing-SubArrays-O(n).py
@@ -0,0 +1,22 @@
+"""
+March 30, 2020
+Optimised code to count the number of subarrays in a given Array.
+Time Complexity: O(n)
+"""
+def increasingSubArray(N,A):
+    count = N
+    length = 1
+    for i in range(N-1):
+        if A[i+1]>A[i]:
+            length+=1
+        else:
+            len = 1
+            count += (((legth-1)*length)//2)
+    if len>1:
+        count += (((legth-1)*length)//2)
+    break
+
+if "__name__" == "__main__":
+    N = int(input())
+    A = list(map(int,input().split(" ")))
+    print(increasingSubArray(N,A))
