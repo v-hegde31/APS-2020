1e058918c9764805944a31c1b9949c8f9e3ce6e4
v-hegde31 <39787460+v-hegde31@users.noreply.github.com>
2020-04-07 12:56:48 +0530

    Create 66-String-Upper-to-Lower-case-conversion.py

---
+++ CodeLib/66-String-Upper-to-Lower-case-conversion.py
@@ -0,0 +1,14 @@
+"""
+Upper to lower case conversions
+Tue, Apr 7, 2020
+"""
+
+s = input()
+
+#method1: Using Ascii
+for i in s:
+    i = chr(ord(i)|32)
+    print(i)
+    
+#method2: Using string.lower() API
+print(s.lower())
